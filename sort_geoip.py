b0a5986f87a053f5cf98f57481638b9239362bfc
gijack <iamjacksemail@hackermail.com>
2015-05-05 14:40:09 -0400

    added DNS reverse DNS for the IP addresses

--- sort_geoip.py
+++ sort_geoip.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import dns.reversename
+
 #read the file
 inFile    = open("traces.geoip","r")
 fileLines = str(inFile.read())
@@ -7,6 +9,7 @@ inFile.close()
 fileLines =  fileLines.split('\n')
 
 filterLines = []
+DNSdict = {}        
 
 #get lines that have locations
 for line in fileLines:
@@ -18,12 +21,31 @@ for line in fileLines:
             filterLines.append(line)
     except:
         continue
+    #now try DNS lookups:
+    #try:
+    try:
+        addr = dns.reversename.from_address( line[0] )
+        DNSdict[line[0]] = addr.to_text()
+    except:
+        continue
 
 #now output
-print("IP Address" + "\t".expandtabs(15) + "Location")
-print("---------------------------------------------")
+print("IP Address" + "\t".expandtabs(6) + "Hostname" + "\t".expandtabs(25) + "Location")
+print("-------------------------------------------------------------------------------")
 for line in filterLines:
-    output = line[0] + "\t".expandtabs(24 - len(line[0]))
+    #start with the IP address and a tab
+    output = line[0] + "\t".expandtabs(16 - len(line[0]))
+    
+    #next is hostname
+    if line[0] in DNSdict:
+        output += DNSdict[line[0]]
+        dnslen = len(DNSdict[line[0]])
+    else:
+        output += "unresolved"
+        dnslen = 10
+    output += "\t".expandtabs(32 - dnslen)
+
+    #third we print whatever location information is available.
     if line[1] == "US":
         if "N/A" not in line[3]:
             output +=        line[3]
