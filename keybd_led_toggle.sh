aa7a8c6229d63cab790da2b6555bacf6c04bad94
devninja <ninja_os@yahoo.com>
2016-04-18 09:12:15 -0400

    consoldated awk

--- keybd_led_toggle.sh
+++ keybd_led_toggle.sh
@@ -9,7 +9,7 @@ declare FLAGS
 # get status of LED as a series of flags. the last set of numbers tends to vary
 # with caps and numlock set. scroll lock is locked to the LED key and won't
 # toggle normally. It will only toggle with the LEDs set with xset.
-FLAGS=$(xset -q | awk 'NR==2' | awk '{ print $10 }')
+FLAGS=$(xset -q | awk 'NR==2{print $10}')
 case $FLAGS in
   0000000?)
     xset led on
