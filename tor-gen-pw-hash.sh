9c502b9e946e977cb8d05fee2c472fcfecab318c
GI_Jack <GI_Jack@hackermail.com>
2020-06-23 22:20:26 -0400

    now with more readline

--- tor-gen-pw-hash.sh
+++ tor-gen-pw-hash.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
-[ -z $1 ] && echo "specify a password";exit 1
-tor --hash-password "${@}"
+#!/usr/bin/env bash
+# Generate a hashed password for TOR remote controll
+password="${@}"
+
+while [ -z ${password} ];do
+  read -p "Enter new TOR Password: " password
+done
+
+tor --hash-password "${password}"
