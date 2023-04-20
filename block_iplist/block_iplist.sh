fd7ad7c01e7a9ed34b5b9493466aa77f0422791b
GI_Jack <GI_Jack@hackermail.com>
2023-04-19 19:24:09 -0700

    Instead of just one line at a time, legit rate limited for iptables in shell using some interesting conditional logic for wait

--- block_iplist/block_iplist.sh
+++ block_iplist/block_iplist.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+# © Copyright GPL v3 GI_Jack 2023
+#exit codes 0-success, 1-operational error, 2-conditional error, 4-help and exit
 
 ### CONFIG
 # One IP per line, no comments
 BLOCK_LIST_FILE=/etc/iptables/block_ips.txt
 BLOCK_CHAIN=DROP #If you are running something like ninja-ids, you can use LOG_DENY
+
+# How many iptables commands can run concurrently. If you increase this, start
+# stop run faster as more rules are added in paralell. But you run into issues
+# with running out of memory and ulimits if set to high
+INSTANCE_RATE_LIMIT=50 
 ### /CONFIG
 
 help_and_exit(){
@@ -33,52 +40,59 @@ warn(){
 }
 
 _start(){
-  local -i errors=0
   local pids=""
   for item in ${BLOCK_LIST};do
-    iptables -I INPUT -s "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -I INPUT -s "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    iptables -I OUTPUT -d "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -I OUTPUT -d "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    iptables -I FORWARD -s "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -I FORWARD -s "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    iptables -I FORWARD -d "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -I FORWARD -d "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    wait ${pids}
-    pids=""
+    instances=$(ps aux |grep iptables |wc -l)
+    if [ ${instances} -ge ${INSTANCE_RATE_LIMIT} ];then
+      wait ${pids}
+      pids=""
+    fi
   done
   
+  # final wait for pids
+  wait ${pids}
   
   return ${errors}
 }
 
 _stop(){
-  local -i errors=0
   local pids=""
+  local -i instances=0
   for item in ${BLOCK_LIST};do
-    iptables -D INPUT -s "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -D INPUT -s "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    iptables -D OUTPUT -d "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -D OUTPUT -d "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    iptables -D FORWARD -s "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -D FORWARD -s "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    iptables -D FORWARD -d "${item}" -j "${BLOCK_CHAIN}" || errors+=1 &
+    iptables -D FORWARD -d "${item}" -j "${BLOCK_CHAIN}" || ERRORS+=1 &
     pids+="${!} "
-    wait ${pids}
-    pids=""
+    instances=$(ps aux |grep iptables |wc -l)
+    if [ ${instances} -ge ${INSTANCE_RATE_LIMIT} ];then
+      wait ${pids}
+      pids=""
+    fi
   done
-  return ${errors}
+  
+  # final wait for pids remaining
+  wait ${pids}
 }
 
 _reload(){
-  local -i errors=0
-  _stop || errors+=${errors}
-  _start || errors+=${errors}
-  return ${errors}
+  _stop
+  _start
 }
 
 main() {
-   local -i errors=0
+   declare -i errors=0
    local command="${1}"
    message "Bulk IP Address Block Tool. Loading ips from: ${BLOCK_LIST_FILE}"
    BLOCK_LIST="$(cat ${BLOCK_LIST_FILE})" || exit_with_error 1 "Could not read Block List File ${BLOCK_LIST_FILE}. Ensure this file exists and is readable, and try again"
@@ -89,22 +103,22 @@ main() {
    case "${command}" in
      start)
        submsg "Blocking IPs"
-       _start || errors+=${?}
+       _start
        ;;
      stop)
        submsg "Removing IP Blocks"
-       _stop || errors+=${?}
+       _stop
        ;;
      reload)
        submsg "Reloading IP Block rules"
-       _reload || errors+=${?}
+       _reload
        ;;
     *)
       help_and_exit
        ;;
    esac
    
-   case ${errors} in
+   case ${ERRORS} in
      0)
        message "Done"
        exit 0
